e0c7286574cd00bcd41e5a7297d2cbf3293d9310
OursBleu <marc.garbage@gmail.com>
2019-04-27 18:04:27 +0100

    Prototype flow

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialog", menuName = "Dialog", order = 1)]
+public class Dialog : ScriptableObject
+{
+    [TextArea]
+    public string Question = "Est-ce que tu approuves ?";
+}
