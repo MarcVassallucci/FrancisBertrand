08a76f49d92ba5a09b0dc39b40867a7cfc3d6058
guimero64 <49920408+guimero64@users.noreply.github.com>
2019-04-29 12:22:31 +0400

    Added text to credits and button. Added eye raw image placeholder to title screen.

---
+++ Assets/Scripts/OnButtonPress.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnButtonPress : MonoBehaviour
+{
+
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+
+  
+}
